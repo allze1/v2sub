67e9d189e671e39c51e1db1ea204823cc0a24704
runoneall <runoobsteve@gmail.com>
2026-06-16 13:52:53 +0800

    feat: add GET endpoint for listing names from environment data

--- src/source.js
+++ src/source.js
@@ -7,4 +7,10 @@ source.use(needauth)
 import { getenv } from './cfenv.js'
 const env = getenv()
 
+source.get('/list', async (req, res) => {
+    const keys = (await env.data.list()).keys
+    const names = keys.map(item => item.name)
+    return res.status(200).json(names)
+})
+
 export default source
